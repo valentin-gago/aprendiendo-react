c0d76b178b1c0caded955006a25a82e3395ca224
Valentin Gago <valentin.gago@hotmail.es>
2023-09-28 20:22:47 +0200

    add segundo commit

--- proyectos/03-mouse-follower/src/App.jsx
+++ proyectos/03-mouse-follower/src/App.jsx
@@ -1,9 +1,34 @@
-
+import { useEffect,useState } from "react";
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  
+  useEffect(() => {
+   console.log("effect", {enabled})
+  },[enabled])
+
+
   return (
-    <h1>proyecto 3</h1>
-  )
+    <>
+      <main>
+        <div style={{
+          position: "absolute",
+          background: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: "50%",
+          top: "50%",
+          width: 40,
+          height:40,
+          transform: "translate(0px, 0px)"
+        }}></div>
+         <button onClick={() => setEnabled(!enabled)} >
+        {enabled? "activar" : "desactivar"} seguir puntero
+        </button>
+      </main>
+    </>
+  );
     
 }
 
